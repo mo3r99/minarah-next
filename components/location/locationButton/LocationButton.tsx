0ccafc5be53755dd696c13b9869197ba8d42a537
Muhammad Rauf <mo3r99@gmail.com>
2025-08-14 12:33:03 +0100

    Make the location show in the button then disable it once done

--- components/location/locationButton/LocationButton.tsx
+++ components/location/locationButton/LocationButton.tsx
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, buttonVariants } from "../../ui/button";
 import { LoaderCircle, MapPin, MapPinOff } from "lucide-react";
 import useLocation from "@/lib/hooks/useLocation";
+
+import type { Location } from "@/types";
 
 import { VariantProps } from "class-variance-authority";
 
 function LocationButton({ ...props } : React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants>) {
   const { loading, error, requestLocation } = useLocation();
+  const [locationValue, setLocationValue] = useState<Location | undefined>(undefined);
 
   function locationClickHandle() {
-    requestLocation();
+    requestLocation().then(location => setLocationValue(location ? location : undefined)).catch(err => console.warn(err));
   }
 
   return (
-    <Button disabled={loading || typeof error == "string"} onClick={locationClickHandle} {...props}>
+    <Button disabled={loading || typeof error == "string" || typeof locationValue != "undefined" } onClick={locationClickHandle} {...props}>
       {loading && <LoaderCircle className="animate-spin" />}
       {error && <MapPinOff />}
       {!loading && !error && <MapPin />}
-      {!error ? 'Get Location' : 'Unable to Get Location' }
+      {!error && !locationValue ? 'Get Location' : locationValue ? locationValue.address?.substring(0, 18) + '...' : error && 'Unable to Get Location' }
     </Button>
   );
 }
