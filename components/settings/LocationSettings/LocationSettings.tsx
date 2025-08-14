d693c82cc1a57a4cdd99ac5ee2ff510a19f70a67
Muhammad Rauf <mo3r99@gmail.com>
2025-08-14 12:35:06 +0100

    fix build error

--- components/settings/LocationSettings/LocationSettings.tsx
+++ components/settings/LocationSettings/LocationSettings.tsx
@@ -8,7 +8,7 @@ import React, { useContext } from "react";
 export default function LocationSettings({...props}:React.ComponentProps<"div">) {
   const { location } = useContext(LocationContext);
   return (
-    <>
+    <div {...props}>
       <p>Current Location: {location.address}</p>
       <p className="mt-4 font-semibold">Update location</p>
       <div className="flex flex-col w-full mx-auto max-w-[250px] gap-2 flex-1/2 items-stretch text-center mt-2">
@@ -16,6 +16,6 @@ export default function LocationSettings({...props}:React.ComponentProps<"div">)
         <span className="text-sm text-zinc-500">OR</span>
         <PlacesAutocomplete className={'mx-auto'}/>
       </div>
-    </>
+    </div>
   );
 }
