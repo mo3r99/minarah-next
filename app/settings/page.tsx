fadbed82ab333d2bea2ca8cdcb9c8bea0b82584a
Muhammad Rauf <mo3r99@gmail.com>
2025-08-14 12:33:24 +0100

    settings page

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,5 +1,18 @@
+import LocationSettings from "@/components/settings/LocationSettings/LocationSettings";
+import { MapPin, SettingsIcon } from "lucide-react";
+
 export default function SettingsPage() {
-    return (
-        <h1>Settings</h1>
-    )
+  return (
+    <>
+      <h1 className="font-[family-name:var(--font-figtree)] font-medium text-2xl flex gap-2 items-center">
+        <SettingsIcon />
+        Settings
+      </h1>
+      <h2 className="font-[family-name:var(--font-figtree)] font-semibold text-xl flex gap-2 items-center mt-4">
+        <MapPin width={16} />
+        Location
+      </h2>
+      <LocationSettings className="mt-2"/>
+    </>
+  );
 }
