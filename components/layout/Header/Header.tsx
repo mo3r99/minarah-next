372cbda0277406a7382953e24e58078855ea6f2d
Muhammad Rauf <mo3r99@gmail.com>
2025-08-14 12:33:30 +0100

    change logo

--- components/layout/Header/Header.tsx
+++ components/layout/Header/Header.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image';
-import MinarahLogo from '@/public/logos/minarah_logo.png';
+import MinarahLogo from '@/public/logos/logo.svg';
 
 export default function Header() {
     return (
         <div className='flex items-center justify-center p-2 font-[family-name:var(--font-figtree)]'>
             <div className='flex items-center'>
-                <Image alt={'logo'} src={MinarahLogo} className='w-3' width={12} height={43}/>
-                <span className='font-display tracking-widest font-medium text-lg pl-2 pt-1 text-[#2e7764]'>MINARAH</span>
+                <Image alt={'logo'} src={MinarahLogo} className='w-6' width={24} height={29}/>
+                <span className='font-display tracking-widest font-medium ml-1 text-lg pt-1 text-[#2e7764]'>MANARAH</span>
             </div>
         </div>
     )
