a36c487a3da145ff90ddb1aab9195b7f8c829887
Muhammad Rauf <mo3r99@gmail.com>
2025-08-14 16:56:49 +0100

    get distance between two places

--- lib/utils/utils.ts
+++ lib/utils/utils.ts
@@ -1,20 +1,25 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-export function timeRemainingToSalahAsString(remainingTime: number | undefined, nextPrayerName: string | undefined) {
-const [hours, minutes] = ((remainingTime || 0) / 60).toString().split('.');
+export function timeRemainingToSalahAsString(
+  remainingTime: number | undefined,
+  nextPrayerName: string | undefined
+) {
+  const [hours, minutes] = ((remainingTime || 0) / 60).toString().split(".");
   const formattedHours = parseInt(hours);
-  const formattedMinutes = Math.round(((parseFloat(`0.${minutes || '0'}`) * 60)));
+  const formattedMinutes = Math.round(parseFloat(`0.${minutes || "0"}`) * 60);
 
   const timeString = {
-    prayer: `${nextPrayerName?.substring(0,1).toUpperCase()}${nextPrayerName?.substring(1)}`,
-    hours: '',
-    and: '',
-    minutes: ''
+    prayer: `${nextPrayerName
+      ?.substring(0, 1)
+      .toUpperCase()}${nextPrayerName?.substring(1)}`,
+    hours: "",
+    and: "",
+    minutes: "",
   };
 
   if (formattedHours > 1) {
@@ -24,16 +29,41 @@ const [hours, minutes] = ((remainingTime || 0) / 60).toString().split('.');
   }
 
   if (formattedHours > 0 && formattedMinutes > 1) {
-    timeString.and = ' and ';
+    timeString.and = " and ";
   } else {
-    timeString.and = ''
+    timeString.and = "";
   }
 
   if (formattedMinutes > 1) {
-    timeString.minutes = `${formattedMinutes} minutes`
+    timeString.minutes = `${formattedMinutes} minutes`;
   } else if (formattedMinutes == 1) {
     timeString.minutes = `a minute`;
   }
 
-  return `${timeString.prayer} begins in ${timeString.hours}${timeString.and}${timeString.minutes}`
+  return `${timeString.prayer} begins in ${timeString.hours}${timeString.and}${timeString.minutes}`;
+}
+
+function getDistanceFromLatLonInKm(
+  lat1: number,
+  lon1: number,
+  lat2: number,
+  lon2: number
+) {
+  // the shortest distance over the earth’s surface between two points using the ‘Haversine’ formula
+  const r = 6371; // radius of earth in km
+  const p = Math.PI / 180;
+
+  const a =
+    0.5 -
+    Math.cos((lat2 - lat1) * p) / 2 +
+    (Math.cos(lat1 * p) *
+      Math.cos(lat2 * p) *
+      (1 - Math.cos((lon2 - lon1) * p))) /
+      2;
+
+  return 2 * r * Math.asin(Math.sqrt(a));
+}
+
+function deg2rad(deg: number) {
+  return deg * (Math.PI / 180);
 }
