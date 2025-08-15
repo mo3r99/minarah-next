4cee2a320b5b754bdcc33ae72e4719def2184461
Muhammad Rauf <mo3r99@gmail.com>
2025-08-15 16:49:54 +0100

    add app version to bottom

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,12 +1,14 @@
 import LocationSettings from "@/components/settings/LocationSettings/LocationSettings";
 import ThemeToggle from "@/components/settings/ThemeToggle/ThemeToggle";
 import { Button } from "@/components/ui/button";
+import { getAppVersion } from "@/lib/utils/utils";
 import { HeartHandshake, MapPin, SettingsIcon, SunMoon } from "lucide-react";
+import Link from "next/link";
 
 export default function SettingsPage() {
   return (
     <>
-      <h1 className="font-[family-name:var(--font-figtree)] font-medium text-2xl flex gap-2 items-center">
+      <h1 className="font-[family-name:var(--font-figtree)] font-medium text-2xl flex gap-2 items-center mt-4">
         <SettingsIcon />
         Settings
       </h1>
@@ -27,25 +29,57 @@ export default function SettingsPage() {
         <ThemeToggle className="mt-2" />
       </div>
 
-      <div className="mb-24 max-w-xl">
+      <div className="max-w-xl">
         <h2 className="font-[family-name:var(--font-figtree)] font-semibold text-xl flex gap-2 items-center mt-6">
           <HeartHandshake width={16} />
           Support Manarah
         </h2>
         <p className="mt-2">
-          Manarah thrives with the support of its community. Your contributions help us add new features, expand our mosque listings, and continue building tools that serve the ummah. Support our mission and be part of the journey.
+          Manarah thrives with the support of its community. Your contributions
+          help us add new features, expand our mosque listings, and continue
+          building tools that serve the ummah. Support our mission and be part
+          of the journey.
         </p>
 
         <p className="font-semibold mt-4">Choose your contribution</p>
 
         <div className="flex flex-row gap-4 flex-wrap mt-4">
-          
-          {[1, 2, 5, 10, 15, 20].map(price => {
+          {[1, 2, 5, 10, 15, 20].map((price) => {
             return (
-              <Button variant={'secondary'} key={price}>£{price}</Button>
-            )
+              <Button variant={"secondary"} key={price}>
+                £{price}
+              </Button>
+            );
           })}
         </div>
+
+        <p className="font-semibold mt-4">Are you a developer?</p>
+        <p className="mt-2">
+          Why not contribute to the building of Manarah on{" "}
+          <Link
+            className="text-blue-600"
+            href={"https://www.github.com/mo3r99/minarah-next"}
+            target="_blank"
+          >
+            GitHub
+          </Link>
+          ?
+        </p>
+      </div>
+
+      <div className="mb-24 mt-8 max-w-xl text-center">
+        <p className="text-sm font-light">
+          Minrah v{getAppVersion()}
+        </p>
+        <p className="text-sm font-light">
+          Brought to you by{" "}
+          <Link className="text-blue-600" href="https://www.almasjid.co.uk/">
+            Al Masjid
+          </Link>
+        </p>
+        <p className="text-sm font-light mt-2">
+          © 2025 Al Masjid SCIO 2015-2025. All rights reserved.
+        </p>
       </div>
     </>
   );
