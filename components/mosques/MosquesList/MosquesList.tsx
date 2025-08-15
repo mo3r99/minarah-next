fd1d04580111bd20cf48360d202dfb6fc63bac75
Muhammad Rauf <mo3r99@gmail.com>
2025-08-15 16:51:08 +0100

    List of mosques

--- components/mosques/MosquesList/MosquesList.tsx
+++ components/mosques/MosquesList/MosquesList.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+'use client'
 
-export default function MosquesList() {
+import { PrayerTimeContext } from '@/lib/context/prayerTimeContext'
+import React, { useContext } from 'react'
+import MosqueItem from '../MosqueItem/MosqueItem';
+import { getDistanceFromLatLonInKm } from '@/lib/utils/utils';
+import { LocationContext } from '@/lib/context/locationContext';
+
+export default function MosquesList({className, ...props}:React.ComponentProps<"div">) {
+  const { location } = useContext(LocationContext);
+  const { mosques } = useContext(PrayerTimeContext);
+ 
+  const mosqueList = mosques.map(mosque => mosque); // Make a copy of the array so that sorting does not mutate the original
+  mosqueList.sort((a,b) => {
+    a.distance = getDistanceFromLatLonInKm(location.coordinates.latitude, location.coordinates.longitude, a.coordinates.latitude, a.coordinates.longitude)
+    b.distance = getDistanceFromLatLonInKm(location.coordinates.latitude, location.coordinates.longitude, b.coordinates.latitude, b.coordinates.longitude)
+    return a.distance - b.distance
+  })
   return (
-    <div>MosquesList</div>
+    <div className={className} {...props}>
+      {mosqueList.map(mosque => {
+        return <MosqueItem key={mosque.coordinates.latitude+mosque.coordinates.longitude} mosque={mosque} distance={mosque.distance}/>
+      })}
+    </div>
   )
 }
