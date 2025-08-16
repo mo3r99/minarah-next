e8d3249d1933a7bd7a494a17cad5e98ced7baf26
Muhammad Rauf <mo3r99@gmail.com>
2025-08-16 09:43:04 +0100

    add build number generator

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
 import type { NextConfig } from "next";
 
+import packageJson from '@/package.json';
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  images: { unoptimized: true } 
+  images: { unoptimized: true },
+  generateBuildId: async () => {
+    // Use package version + timestamp
+    return `v${packageJson.version}-${Date.now()}`;
+  },
+
 };
 
 export default nextConfig;
